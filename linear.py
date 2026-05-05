047c2fdc2546070c4bf3ddbbd538058432985b58
gopitha20251663-gif <gopitha20251663@miis.ae>
2026-05-05 11:30:57 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -2,7 +2,7 @@ import streamlit as pd
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-df = pd.read_cvs("student_score")
+df = pd.read_cvs("student_score.csv")
 X = df[['HoursStudied']]
 y = df['ExamScore']
 X_train, X_test, y_train,y_test=train_test_split(X, y, test_size=0.2, random_state=42)
@@ -11,4 +11,8 @@ model.fit(X_train, y_train)
 st.title("exam score predictor")
 st.write("Enter house studied to predict the exam score.")
 hours = st.number_input("Hours Studied:", min_value=0.0, step=0.1)
-if st.button("predict score")
+if st.button("predict score"):
+  predicted_score = model.predict([[hours]])[0}
+  st.success(f"predicted Score:{predicted_score:.2f}")
+st.write("### sample training data")
+st.dataframe(df)
